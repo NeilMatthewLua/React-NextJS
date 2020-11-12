d8056f18fe7a267de5a4c3df1390d09bece8e1bf
unknown <neil_lua@dlsu.edu.ph>
2020-11-12 12:48:21 +0800

    Updated tutorial up to polishing index

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../../components/layout'
+import Alert from '../../components/alert'
 
 export default function FirstPost() {
   return (
@@ -10,11 +11,10 @@ export default function FirstPost() {
           <title>First Post</title>
         </Head>
           <h1>First Post</h1>
-          <h2>
-            <Link href="/">
-              <a>Back to home</a>
-            </Link>
-          </h2>
+
+        <Alert type='error'>
+          <h1>Look at that!</h1>
+        </Alert>
       </Layout>
     </>
   )
