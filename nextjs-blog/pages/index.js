1c33683ea042382084daaf22be83596f3bb9107c
NeilMatthewLua <neil_lua@dlsu.edu.ph>
2020-11-13 14:56:36 +0800

    Finished Tutorial

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import { getSortedPostsData } from '../lib/posts'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import Date from './../components/date'
 
 export default function Home ({ allPostsData }) {
   return (
@@ -14,11 +15,13 @@ export default function Home ({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
